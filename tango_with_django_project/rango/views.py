a0e4b4448799619151187eeac5aa6c4b3739eb5d
Aravind <xyperus@gmail.com>
2017-01-25 12:19:21 +0000

    chap 4 done.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -5,9 +5,24 @@ from django.http import HttpResponse
 
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': "A, B, O, U, T!"}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'rango/about.html', context=context_dict)
+
+
+    
 
 def index(request):
-    return HttpResponse("Rango says hey there partner ! <br/> <a href='/rango/about/'>About</a>")
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
 
-
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'rango/index.html', context=context_dict)
