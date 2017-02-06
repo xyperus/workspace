12c4cc6ee88c5893e0c768031f9afca0bc881cd3
Aravind <xyperus@gmail.com>
2017-02-06 16:59:44 +0000

    db populated

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -1,60 +1,62 @@
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','tango_with_django_project.settings')
+
+
+import django
+django.setup()
+from rango.models import Category, Page
 
 def populate():
-    python_cat = add_cat('Python', views=128, likes=64)
+    python_pages = [
+        {"title": "Official Python Tutorial",
+         "url":"http://docs.python.org/2/tutorial/"},
+        {"title":"How to Think like a Computer Scientist",
+         "url":"http://www.greenteapress.com/thinkpython/"},
+        {"title":"Learn Python in 10 Minutes",
+         "url":"http://www.korokithakis.net/tutorials/python/"} ]
 
-    add_page(cat=python_cat,
-        title="Official Python Tutorial",
-        url="http://docs.python.org/2/tutorial/")
+    django_pages = [
+        {"title":"Official Django Tutorial",
+         "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
+        {"title":"Django Rocks",
+         "url":"http://www.djangorocks.com/"},
+        {"title":"How to Tango with Django",
+         "url":"http://www.tangowithdjango.com/"} ]
 
-    add_page(cat=python_cat,
-        title="How to Think like a Computer Scientist",
-        url="http://www.greenteapress.com/thinkpython/")
+    other_pages = [
+        {"title":"Bottle",
+         "url":"http://bottlepy.org/docs/dev/"},
+        {"title":"Flask",
+         "url":"http://flask.pocoo.org"} ]
 
-    add_page(cat=python_cat,
-        title="Learn Python in 10 Minutes",
-        url="http://www.korokithakis.net/tutorials/python/")
+    cats = {"Python": {"pages": python_pages},
+            "Django": {"pages": django_pages},
+            "Other Frameworks": {"pages": other_pages} }
+    
+         
+    for cat, cat_data in cats.items():
+        c = add_cat(cat)
+        for p in cat_data["pages"]:
+            add_page(c, p["title"], p["url"])
 
-    django_cat = add_cat("Django", views=64, likes=32)
-
-    add_page(cat=django_cat,
-        title="Official Django Tutorial",
-        url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/")
-
-    add_page(cat=django_cat,
-        title="Django Rocks",
-        url="http://www.djangorocks.com/")
-
-    add_page(cat=django_cat,
-        title="How to Tango with Django",
-        url="http://www.tangowithdjango.com/")
-
-    frame_cat = add_cat("Other Frameworks", views=32, likes=16)
-
-    add_page(cat=frame_cat,
-        title="Bottle",
-        url="http://bottlepy.org/docs/dev/")
-
-    add_page(cat=frame_cat,
-        title="Flask",
-        url="http://flask.pocoo.org")
-
-    # Print out what we have added to the user.
+    
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
-            print "- {0} - {1}".format(str(c), str(p))
+            print("- {0} - {1}".format(str(c), str(p)))
 
 def add_page(cat, title, url, views=0):
-    p = Page.objects.get_or_create(category=cat, title=title, url=url, views=views)[0]
+    p = Page.objects.get_or_create(category=cat, title=title)[0]
+    p.url=url
+    p.views=views
+    p.save()
     return p
 
-def add_cat(name, views=0, likes=0):
-    c = Category.objects.get_or_create(name=name,views=views,likes=likes)[0]
+def add_cat(name):
+    c = Category.objects.get_or_create(name=name)[0]
+    c.save()
     return c
 
-# Start execution here!
 if __name__ == '__main__':
-    print "Starting Rango population script..."
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
-    from rango.models import Category, Page
+    print("Starting Rango population script...")
     populate()
+    
--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -6,6 +6,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
     def __str__(self): # For Python 2, use __unicode__ too
         return self.name
