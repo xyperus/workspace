9c77b2188fa78e9388696c9dbfb1845357c03dd3
Aravind <xyperus@gmail.com>
2017-02-06 17:49:09 +0000

    excersiceise mostly dun for chap 5

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -29,11 +29,16 @@ def populate():
         {"title":"Flask",
          "url":"http://flask.pocoo.org"} ]
 
-    cats = {"Python": {"pages": python_pages},
+    cats = {"Python": {"pages": python_pages} , {"views": "128"}, {"likes":"64"},
             "Django": {"pages": django_pages},
             "Other Frameworks": {"pages": other_pages} }
-    
-         
+
+#    django_cat = add_cat("Django", "views":"64", "likes":"32")
+
+ #   python_cat = add_cat("Python", "views":"128", likes=64)
+
+  #  frame_cat = add_cat("Other Frameworks", views=32, likes=16)
+
     for cat, cat_data in cats.items():
         c = add_cat(cat)
         for p in cat_data["pages"]:
@@ -51,8 +56,10 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views=views
+    c.likes=likes
     c.save()
     return c
 
