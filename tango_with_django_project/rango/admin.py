a6928fb5b73678089283f8c47b9d296aa2a67056
Aravind <xyperus@gmail.com>
2017-02-11 15:14:59 +0000

    chap 6 dun

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
 # Register your models here.
 from rango.models import Category, Page
 
+class PageAdmin(admin.ModelAdmin):
+        list_display = ('title', 'category', 'url')
 
-admin.site.register(Category)
-admin.site.register(Page)
+# Add in this class to customise the Admin Interface
+class CategoryAdmin(admin.ModelAdmin):
+        prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,5 +1,5 @@
 from __future__ import unicode_literals
-
+from django.template.defaultfilters import slugify
 from django.db import models
 
 # Create your models here.
@@ -8,7 +8,16 @@ class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
 
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    
     def __str__(self): # For Python 2, use __unicode__ too
         return self.name
 
